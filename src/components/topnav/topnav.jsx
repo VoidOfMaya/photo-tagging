30fe1d7efc8b69873325732fbb99d5f6ca3e1bbf
Maya <MayaOfTheVoid@gmail.com>
2026-03-17 14:40:08 +0200

    basic project setup

---
+++ src/components/topnav/topnav.jsx
@@ -0,0 +1,12 @@
+const Topnav =()=>{
+    return(
+        <section>
+            <div>title</div>
+            <div>score board</div>
+        </section>
+    )
+}
+
+export{
+    Topnav
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { NotFound } from './components/404Page/notFound.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { Home } from './components/homepage/homepage.jsx'
 
+const router = createBrowserRouter([
+  {
+  path:'/', element: <App />,
+  children: [
+    {path:'/', element: <Home />}
+  ],
+  errorElement: <NotFound />},
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
