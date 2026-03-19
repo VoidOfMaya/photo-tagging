b6dc3fb476beb4e201bc705de5e305808fc4d516
Maya <MayaOfTheVoid@gmail.com>
2026-03-19 13:23:12 +0200

    feat(target select) implements basic targeting by getting  targed co-ordinates and current canvase size

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,16 +1,19 @@
 import { Dropdown } from '../dropdown/dropdown.jsx'
 import levelOne from '../../assets/photos/Waldo-Underground.jpg'
 import style from './home.module.css'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 const Home =()=>{
+    const imgRef = useRef(null);
     const [position, setPostion] = useState({x: null,y: null})
     const [target, setTarget] = useState(false);
 
     const updatePosition=(e)=>{
         const react = e.target.getBoundingClientRect();
+        const img = imgRef.current.getBoundingClientRect();
         const x = e.clientX - react.left;
         const y = e.clientY - react.top;
-        console.log(`x:${x}, y:${y}`)
+        console.log(`current target: \nx:${x}, y:${y}`)
+        console.log(`canvase size: \nx:${img.width}, y:${img.height}`)
         setPostion({x:x, y:y});
         setTarget(true)
     }
@@ -24,12 +27,25 @@ const Home =()=>{
             </>
         )
     }
+
+    useEffect(()=>{
+        const updateSize=()=>{
+            if(!imgRef.current)return;
+            imgRef.current.getBoundingClientRect();
+        };
+        updateSize()
+        window.addEventListener('resize',updateSize);
+        return ()=> window.removeEventListener('resize',updateSize);
+    },[])
     return(
         <div>
-            <img    
+            
+            <img  
+                ref={imgRef}  
                 src={levelOne}
                 className={style.photoCanvas}    
                 onClick={e=>{updatePosition(e)}}
+                onChange={e=>{targetPostion(e)}}
             ></img>
             <div>x:{position.x}</div>
             <div>Y:{position.y}</div>
