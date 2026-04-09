6bb8f0833f2c4af55cce6eaa608cd1b8bc29c03f
Maya <MayaOfTheVoid@gmail.com>
2026-04-09 14:03:15 +0300

    feat(targets list): implements a visual targetting indecator

--- src/components/targetList/targets.jsx
+++ src/components/targetList/targets.jsx
@@ -1,12 +1,18 @@
 import style from './target.module.css'
-const TargetCard = ({target})=>{
+const TargetCard = ({target, setData})=>{
     return(
         <div style={{padding: '10px'}}
-        className={
-            target.isSelected?(style.found):(style.notFound)
-        }
-        onClick={()=>{select(target.name)}}>
-            {target.name}
+        className={ `${style.base}
+            ${target.isSelected?(style.found):(style.notFound)}`}
+        onClick={()=> setData(target.name)}>
+            <img src={target.photo} 
+                 alt={target.name} 
+                 width='100px'
+                 className={style.photo}>
+            </img>
+            <div>x:{target.coords.x}</div>
+            <div>x:{target.coords.y}</div>
+  
         </div>
     )
 }
